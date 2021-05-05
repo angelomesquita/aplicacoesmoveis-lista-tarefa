58a3d85080524f58408d8ed12de898e6f4e0637d
Angelo Mesquita <angeloamesquita@gmail.com>
2021-05-05 20:12:26 -0300

    Implementacao do layout da splash screen

--- app/src/main/java/br/edu/unis/listadetarefas/activity/SplashActivity.java
+++ app/src/main/java/br/edu/unis/listadetarefas/activity/SplashActivity.java
@@ -17,7 +17,7 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        setTitle("Splash Screen");
+        getSupportActionBar().hide();
         carregando();
     }
 
