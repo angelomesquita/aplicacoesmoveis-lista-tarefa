ba9c48e4dbbfb45d12b66189f3a75c3f75ad269d
Angelo Mesquita <angeloamesquita@gmail.com>
2021-05-05 19:56:02 -0300

    Implementacao da SplashScreen
    Foi criada a activity SplashActivity e programada o tempo de
    carregamento para na sequencia a exibicao da listaTarefaActivity

---
+++ app/src/main/java/br/edu/unis/listadetarefas/activity/SplashActivity.java
@@ -0,0 +1,42 @@
+package br.edu.unis.listadetarefas.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import br.edu.unis.listadetarefas.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    public static final int TEMPO_DE_ATRASO = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        setTitle("Splash Screen");
+        carregando();
+    }
+
+    private void carregando() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                abreListaTarefas();
+            }
+        }, TEMPO_DE_ATRASO);
+    }
+
+
+    private void abreListaTarefas() {
+        Intent intent = new Intent(
+            this,
+            ListaTarefasActivity.class
+        );
+        startActivity(intent);
+    }
+}
